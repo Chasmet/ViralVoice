f8c1d18948b1a8cb1ec5aeb5fe90d9766e6f71f6
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-05-08 12:31:40 +0200

    Ajoute service worker ViralVoice

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'viralvoice-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(response => response || caches.match('./index.html')))
+  );
+});
